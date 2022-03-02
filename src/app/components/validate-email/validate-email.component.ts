35b39945c6b363d0fa8c85847b60d7c72cf7aeb8
Matin <matin.parsapour.iam@gmail.com>
2022-03-02 13:44:54 +0330

    Create a page when user register can verify by this page

---
+++ src/app/components/validate-email/validate-email.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-validate-email',
+  templateUrl: './validate-email.component.html',
+  styleUrls: ['./validate-email.component.css']
+})
+export class ValidateEmailComponent implements OnInit {
+
+  email: any;
+
+  constructor(private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => 
+      {
+      console.log(params['email']);
+      this.email = params['email']
+      })
+  }
+
+  goToLogInPage(){
+    this.router.navigateByUrl("/login")
+  }
+
+}
