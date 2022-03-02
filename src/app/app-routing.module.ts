74ede9bd98d1b321b4b6f327b1994228ff246555
Matin <matin.parsapour.iam@gmail.com>
2022-03-02 13:39:55 +0330

    Update routing for pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ValidateEmailComponent } from './components/validate-email/validate-email.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {path: 'register', component: RegisterComponent},
+  {path: 'validate-email', component: ValidateEmailComponent},
+  {path: '**', component: NotFoundComponent},
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
