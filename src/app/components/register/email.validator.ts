39867e9a5d5b5cbe44b67b40aa91481cb1652f86
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 10:26:01 +0330

    Add an email validator to validate async

---
+++ src/app/components/register/email.validator.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import {
+  AbstractControl,
+  AsyncValidator,
+  ValidationErrors,
+} from '@angular/forms';
+import { map } from 'rxjs';
+import { RegisterService } from './../../services/register/register.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class EmailValidator implements AsyncValidator {
+  constructor(private resgisterService: RegisterService) {}
+  
+  validate = (control: AbstractControl) => {
+    return this.resgisterService.isEmailDoplicate(control.value).pipe(
+      map(
+        (response: any) => {
+          if (response === true) {
+            return { emailIsDoplicate: true };
+          }
+          return null;
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error.message);
+        }
+      )
+    );
+  };
+}
