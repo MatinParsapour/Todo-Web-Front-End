44321c3b102ec4b6bc9940a3954132f0aadf3fc3
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 16:36:48 +0330

    Add a service to send validated user email to serve

---
+++ src/app/services/validateEmail/validate-email.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { DataService } from './../data/data.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ValidateEmailService extends DataService {
+
+  httpService: HttpClient
+  
+  constructor(http: HttpClient) {
+    super("http://localhost:8080/email/", http);
+    this.httpService = http
+  }
+
+  sendValidatedUserEmail(email: any){
+    this.httpService.get('http://localhost:8080/email/verify-email/' + email).subscribe(
+      (response: any) => {
+        console.log(response);
+        
+      }, 
+      (error: HttpErrorResponse) => {
+        console.log(error);
+        
+      }
+    );
+  }
+}
