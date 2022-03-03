030fda0df629514a4610440262f63702c401425f
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 17:13:38 +0330

    Add a service for forget password to connect with server

---
+++ src/app/services/forget-password/forget-password.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './../data/data.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ForgetPasswordService extends DataService {
+
+  httpService: HttpClient
+
+  constructor(http: HttpClient) {
+    super("http://localhost:8080/email/", http);
+    this.httpService = http
+  }
+
+  sendForgetPasswordEmail(email: any){
+    return this.httpService.get("http://localhost:8080/email/forget-password/" + email)
+  }
+}
