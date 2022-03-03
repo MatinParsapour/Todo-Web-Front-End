fae343e01eff9dade6377b065efdab56adf96534
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 10:13:43 +0330

    Add a data service for general requests

---
+++ src/app/services/data/data.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(@Inject(String) private url: string, private http: HttpClient) { }
+
+  getAll(uri: string){
+    return this.http.get(this.url + uri)
+  }
+
+  create(uri: string, object: any){
+    return this.http.post(this.url + uri, object)
+  }
+
+  update(uri: string, object: any){
+    return this.http.put(this.url + uri, object)
+  }
+
+  delete(id: any){
+    return this.http.delete(this.url + id)
+  }
+}
