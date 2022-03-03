124b1721466b83e5d8bb01343b7b65969182ede5
Matin <matin.parsapour.iam@gmail.com>
2022-03-03 10:25:40 +0330

    Add a method to register service to check email

--- src/app/services/register/register.service.ts
+++ src/app/services/register/register.service.ts
@@ -3,19 +3,24 @@ import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RegisterService extends DataService {
-
   constructor(http: HttpClient) {
     super('http://localhost:8080/user/', http);
-    this.httpService = http
+    this.httpService = http;
   }
 
   httpService: HttpClient;
 
-  isUsernameDoplicate(username: any){
-    return this.httpService.get('http://localhost:8081/user/check-username/' + username);
-    };
-  
+  isUsernameDoplicate(username: any) {
+    return this.httpService.get(
+      'http://localhost:8081/user/check-username/' + username
+    );
+  }
+  isEmailDoplicate(email: any) {
+    return this.httpService.get(
+      'http://localhost:8081/user/check-email/' + email
+    );
+  }
 }
