a984c2852f27bce12c34cc3471eb52159eb77ce7
Matin <matin.parsapour.iam@gmail.com>
2022-03-04 12:03:11 +0330

    Add a validator to check if folder is doplicate or not

---
+++ src/app/components/insert-folder/folder-name.validator.ts
@@ -0,0 +1,28 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { InsertFolderService } from './../../services/insert-folder/insert-folder.service';
+import { Injectable } from '@angular/core';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { Observable, map } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FolderNameValidator implements AsyncValidator{
+
+    constructor(private insertFolderService: InsertFolderService){}
+
+    validate = (control: AbstractControl) => {
+        return this.insertFolderService.isFolderNameDoplicate(control.value).pipe(
+            map((response: any) => {
+                if (response === true) {
+                    return {folderNameIsDoplicate: true}
+                } 
+                return null
+            },
+            (error: HttpErrorResponse) => {
+                console.log(error.message);
+            })
+        )
+    }
+    
+}
