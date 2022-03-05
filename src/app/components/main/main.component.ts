bc815878d599616d05f0be5160cca08caaaed9be
Matin <matin.parsapour.iam@gmail.com>
2022-03-05 19:52:48 +0330

    Clear up main component type script

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,3 +1,5 @@
+import { Router } from '@angular/router';
+import { UserComponent } from './../user/user.component';
 import { slideToDown } from './../../animations';
 import { InsertListComponent } from './../insert-list/insert-list.component';
 import { NotificationService } from './../../services/notification/notification.service';
@@ -16,54 +18,88 @@ import { NotificationType } from 'src/app/enum/notification-type';
   animations: [slideToDown]
 })
 export class MainComponent implements OnInit {
+  private GET_TO_DOS_URI = '/user/get-to-dos/' + localStorage.getItem('list') + '/' + localStorage.getItem('folder') + '/' + localStorage.getItem('username');
+  private GET_TO_DO_FOLDERS_URI = '/folder/get-todo-folders/' + localStorage.getItem('username');
+  private CREATE_TO_DO_URI = '/to-do/add-to-do/' + localStorage.getItem('list') + '/folder/' + localStorage.getItem('folder') + '/for/' + localStorage.getItem('username')
   toDoFolders: any;
   toDos: any;
-  
+  user = '';
 
+  toDo = new FormGroup({
+    task: new FormControl(''),
+    dateTime: new FormControl(''),
+  });
 
   constructor(
     private mainService: MainService,
     private dialog: MatDialog,
-    private notifier: NotificationService
+    private notifier: NotificationService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
+    this.user =
+      localStorage.getItem('firstName') +
+      ' ' +
+      localStorage.getItem('lastName');
+    this.getAllToDoFolders();
     this.getAllToDos();
-    this.mainService.getToDos("/user/get-to-dos/" + localStorage.getItem("list") + "/" + localStorage.getItem("folder") + "/" + localStorage.getItem("username")).subscribe(
+  }
+
+  openUserDialog() {
+    this.dialog.open(UserComponent);
+  }
+
+  addAndUpdateToDos() {
+    if (this.task.value.trim() !== '') {
+      this.mainService.create(this.CREATE_TO_DO_URI, this.toDo.value).subscribe(
+        (response: any) => {
+          this.notifier.notify(NotificationType.SUCCESS,'Your to do successfully added');
+          this.getAllToDos();
+          this.task.setValue('')
+        },
+        (error: HttpErrorResponse) => {
+          this.notifier.notify(NotificationType.ERROR, error.message);
+        }
+      );
+    }
+  }
+
+  get task(): any {return this.toDo.get('task');}
+
+  get dateTime(): any {return this.toDo.get('dateTime');}
+
+  getAllToDos() {
+    this.mainService.getToDos(this.GET_TO_DOS_URI).subscribe(
       (response: any) => {
-        response.toDoFolders.forEach((folder:any) => {
-          folder.toDoLists.forEach((list:any) => {
-            this.toDos = list.toDos;            
+        response.toDoFolders.forEach((folder: any) => {
+          folder.toDoLists.forEach((list: any) => {
+            this.toDos = list.toDos;
           });
         });
-        
       },
       (error: HttpErrorResponse) => {
         console.log(error);
-        
       }
-    )
+    );
   }
 
-
-  getAllToDos() {
-    this.mainService
-      .getAll('/folder/get-todo-folders/' + localStorage.getItem('username'))
-      .subscribe(
-        (response: any) => {
-          this.toDoFolders = response;
-        },
-        (error: HttpErrorResponse) => {
-          console.log(error);
-        }
-      );
+  getAllToDoFolders() {
+    this.mainService.getAll(this.GET_TO_DO_FOLDERS_URI).subscribe(
+      (response: any) => {
+        this.toDoFolders = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
   }
 
   openAddListDialog(folderName: any) {
     this.dialog
       .open(InsertListComponent, { data: { folderName: folderName } })
       .afterClosed()
-      .subscribe(() => this.getAllToDos());
+      .subscribe(() => this.getAllToDoFolders());
   }
 
   openCreateFolderDialog() {
@@ -71,41 +107,31 @@ export class MainComponent implements OnInit {
       .open(InsertFolderComponent)
       .afterClosed()
       .subscribe(() => {
-        this.getAllToDos();
+        this.getAllToDoFolders();
       });
   }
 
-  deleteList(folderName: any, listName: any){
-    this.mainService.delete("/list/delete-list/" + listName + "/" + folderName + "/" + localStorage.getItem('username')).subscribe(
-      (response: any) => {
-        this.notifier.notify(NotificationType.SUCCESS, "The list successfully deleted")
-        this.getAllToDos()
-      }, 
-      (error: HttpErrorResponse) => {
-        this.notifier.notify(NotificationType.ERROR, error.message)
-        this.getAllToDos()        
-      }
-    )
-  }
-
-  deleteFolder(folderName: any) {
+  deleteList(folderName: any, listName: any) {
     this.mainService
       .delete(
-        '/folder/delete-folder/' +
-          localStorage.getItem('username') +
+        '/list/delete-list/' +
+          listName +
           '/' +
-          folderName
+          folderName +
+          '/' +
+          localStorage.getItem('username')
       )
       .subscribe(
         (response: any) => {
           this.notifier.notify(
             NotificationType.SUCCESS,
-            folderName + ' successfully deleted'
+            'The list successfully deleted'
           );
-          this.getAllToDos();
+          this.getAllToDoFolders();
         },
         (error: HttpErrorResponse) => {
           this.notifier.notify(NotificationType.ERROR, error.message);
+          this.getAllToDoFolders();
         }
       );
   }
