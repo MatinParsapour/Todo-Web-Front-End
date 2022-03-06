fe111b6cca8c84572a667dd14dcccbfb52be86b4
Matin <matin.parsapour.iam@gmail.com>
2022-03-06 20:12:19 +0330

    Update user component to add or change image profile

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -13,18 +13,22 @@ import { User } from 'src/app/classes/user';
 })
 export class UserComponent implements OnInit {
   user!: User;
+  isLoading: boolean = false;
+  public profileImage: any;
 
   constructor(
     private userService: UserService,
     private notifier: NotificationService
   ) {}
 
-  ngOnInit(): void { this.getUser()}
+  ngOnInit(): void {
+    this.getUser();
+  }
 
   getUser() {
     this.userService.getUser(localStorage.getItem('username')).subscribe(
-      (response: any) => {
-        this.user = response        
+      (response: any) => {        
+        this.user = response;
       },
       (error: HttpErrorResponse) => {
         console.log(error);
@@ -36,27 +40,63 @@ export class UserComponent implements OnInit {
       }
     );
   }
+  changeProfileImage(event: any) {
+    this.profileImage = event.target.files[0];
+    const formData = new FormData();
+    const username = localStorage.getItem('username');
+    if (username != null) {
+      formData.append('username', username);
+    }
+    formData.append('profileImage', this.profileImage);
+    this.userService.update('/user/update-profile-image', formData).subscribe(
+      (response: any) => {
+        this.notifier.notify(
+          NotificationType.SUCCESS,
+          'You profile successfully changed'
+        );
+        this.getUser();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
 
-
-  updateUser(){
+  updateUser() {
     let date = new Date(this.user.birthDay);
     let pipe: DatePipe = new DatePipe('en-US');
-    let birthday = pipe.transform(date,'yyyy-MM-dd')
-    
-    this.user.birthDay = birthday
-    this.userService.update("/user/update-user",this.user).subscribe(
+    let birthday = pipe.transform(date, 'yyyy-MM-dd');
+
+    this.user.birthDay = birthday;
+    this.isLoading = true;
+    this.userService.update('/user/update-user', this.user).subscribe(
       (response: any) => {
-        this.notifier.notify(NotificationType.SUCCESS, "You information updated")
+        this.notifier.notify(
+          NotificationType.SUCCESS,
+          'You information updated'
+        );
         this.user = response;
+        this.isLoading = false;
       },
       (error: HttpErrorResponse) => {
         if (error.status === 406) {
-          this.notifier.notify(NotificationType.ERROR, "The username you entered already exists")        
+          this.notifier.notify(
+            NotificationType.ERROR,
+            'The username you entered already exists'
+          );
         } else {
-          this.notifier.notify(NotificationType.ERROR, "Something went wrong")        
+          this.notifier.notify(NotificationType.ERROR, 'Something went wrong');
         }
+        this.isLoading = false;
       }
-    )
-    
+    );
+  }
+
+  selectProfile(){
+    this.clickButton('select-picture');
+  }
+
+  clickButton(tagId: any){
+    document.getElementById(tagId)?.click()
   }
 }
