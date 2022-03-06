53d68bb1084f37c8e86bf2c2426cb886517af33d
Matin <matin.parsapour.iam@gmail.com>
2022-03-06 21:18:08 +0330

    Update edit to do component now user can update and delete to do

--- src/app/components/edit-to-do/edit-to-do.component.ts
+++ src/app/components/edit-to-do/edit-to-do.component.ts
@@ -1,21 +1,61 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { ToDoService } from './../../services/to-do/to-do.service';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ToDo } from './../../classes/todo';
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-edit-to-do',
   templateUrl: './edit-to-do.component.html',
-  styleUrls: ['./edit-to-do.component.css']
+  styleUrls: ['./edit-to-do.component.css'],
 })
 export class EditToDoComponent implements OnInit {
+  toDo: ToDo = new ToDo();
+  minDate = new Date()
 
-  toDo: ToDo = new ToDo
-
-  constructor(@Inject(MAT_DIALOG_DATA) data: any) {
-    this.toDo = data.todo
+  constructor(
+    @Inject(MAT_DIALOG_DATA) data: any,
+    private toDoService: ToDoService,
+    private notifier: NotificationService
+  ) {
+    this.toDo = data.todo;
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  updateToDo() {
+    let date = new Date(this.toDo.dateTime);
+    let pipe: DatePipe = new DatePipe('en-US');
+    let dueToDate = pipe.transform(date, 'yyyy-MM-dd');
+    this.toDo.dateTime = dueToDate
+    console.log(this.toDo);
+    
+    this.toDoService.update('to-do/update-to-do',this.toDo).subscribe(
+      (response: any) => {
+        this.notifier.notify(NotificationType.SUCCESS, "Your to do updated")
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+        
+        this.notifier.notify(NotificationType.ERROR, error.message)
+      }
+    )
   }
 
+  toggleStar() {
+    this.toDo.isStarred = !this.toDo.isStarred;
+  }
+
+  toggleMyDay() {
+    this.toDo.isMyDay = !this.toDo.isMyDay;
+  }
 }
