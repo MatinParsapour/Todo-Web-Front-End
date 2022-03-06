1062e8449cc5565d1e16b176debfba52baeae433
Matin <matin.parsapour.iam@gmail.com>
2022-03-06 13:30:01 +0330

    Add a class for user

---
+++ src/app/classes/user.ts
@@ -0,0 +1,10 @@
+export class User{
+    id: number = 0;
+    firstName: string = "";
+    lastName: string = "";
+    username: string = "";
+    email: string = "";
+    phoneNumber: number = 0;
+    birthDay: any;
+    age: string = "";
+}
