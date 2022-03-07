0ee47a2294b2a0c4977d92a052ebece95424f2b7
Matin <matin.parsapour.iam@gmail.com>
2022-03-07 16:16:33 +0330

    Add a validtor to check if email exists or not

---
+++ src/app/components/get-reset-email/is-email-exists.validator.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RegisterService } from './../../services/register/register.service';
+import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class IsEmailExists implements AsyncValidator {
+
+    constructor(private registerService: RegisterService){}
+
+    validate = (control: AbstractControl) => {
+        return this.registerService.isEmailDoplicate(control.value).pipe(
+          map(
+            (response: any) => {
+              if (response === true) {
+                return { emailExists: true };
+              }
+              return null;
+            },
+            (error: HttpErrorResponse) => {
+              console.log(error);
+            }
+          )
+        );
+    }
+    
+}
