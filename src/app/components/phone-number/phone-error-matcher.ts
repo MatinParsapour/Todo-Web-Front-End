270620abd045b3745ae46170c4b715044cefe6af
Matin <matin.parsapour.iam@gmail.com>
2022-03-07 13:55:57 +0330

    Check if phone has error

---
+++ src/app/components/phone-number/phone-error-matcher.ts
@@ -0,0 +1,14 @@
+import { AbstractControl, FormGroupDirective, NgForm } from "@angular/forms";
+import { ErrorStateMatcher } from "@angular/material/core";
+
+export class PhoneErrorMatcher implements ErrorStateMatcher {
+    isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
+        return !!(
+          control?.value &&
+          control.touched &&
+          !control?.parent?.valid &&
+          control.invalid
+        );
+    }
+    
+}
