34295842fb3227d75a567004995a89db4c2ab335
Matin <matin.parsapour.iam@gmail.com>
2022-03-08 21:44:01 +0330

    update type of id

--- src/app/classes/todo.ts
+++ src/app/classes/todo.ts
@@ -1,9 +1,10 @@
 export class ToDo {
   dateTime: any = null;
-  id: number = 0;
+  id: any;
   isMyDay: boolean = false;
   isStarred: boolean = false;
   note: string = "";
   status: string = '';
   task: string = '';
+  pictures: any;
 }
