35c2acfc156a203b6e14a7ca8c345b45e4ecf69d
Matin <matin.parsapour.iam@gmail.com>
2022-03-08 21:45:20 +0330

    Add a method to get a specific to do

--- src/app/services/to-do/to-do.service.ts
+++ src/app/services/to-do/to-do.service.ts
@@ -7,7 +7,14 @@ import { Injectable } from '@angular/core';
 })
 export class ToDoService extends DataService{
 
+  httpService: HttpClient
+
   constructor(http: HttpClient) {
     super("http://localhost:8080/",http)
+    this.httpService = http
+   }
+
+   getToDo(uri:any){
+    return this.httpService.get("http://localhost:8080/" + uri)     
    }
 }
