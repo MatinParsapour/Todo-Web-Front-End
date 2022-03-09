a2542011bf0f8f14fdc30245cdf315f8308d6f5c
Matin <matin.parsapour.iam@gmail.com>
2022-03-09 11:19:17 +0330

    Add a component to show an image full screen

---
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-image-viewer',
+  templateUrl: './image-viewer.component.html',
+  styleUrls: ['./image-viewer.component.css']
+})
+export class ImageViewerComponent implements OnInit {
+
+  @Input('open') open: boolean = false;
+  @Input('imageUrl') imageUrl = "";
+  @Output('click') click = new EventEmitter()
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
