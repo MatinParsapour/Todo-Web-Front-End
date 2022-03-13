e0d7352272a934010766628b34f8c256c8757fa1
Matin <matin.parsapour.iam@gmail.com>
2022-03-13 21:15:06 +0330

    Update summary pipe to get number of character user wants to disappear

--- src/app/pipes/summary-pipes.pipe.ts
+++ src/app/pipes/summary-pipes.pipe.ts
@@ -1,24 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'summaryPipes'
+  name: 'summaryPipes',
 })
 export class SummaryPipesPipe implements PipeTransform {
-
-  transform(value: string, ...args: unknown[]): unknown {
+  transform(value: string, limit?: number): unknown {
     if (!value) {
       return null;
     }
 
-    let extension = ""
-    extension = value.split('.').splice(-1)[0]
-    value = value.replace("." + extension, "")
+    let distance = limit ? limit : 10;
+    let extension = '';
+    if (value.includes('.')) {
+      extension = value.split('.').splice(-1)[0];
+      value = value.replace('.' + extension, '');
 
-    if (value.length <=10) {
-      return value + "." + extension
+      if (value.length <= 10) {
+        return value + '.' + extension;
+      }
+      return value.substring(0, distance) + '... ' + '.' + extension;
     }
 
-    return value.substring(0,10) + '... ' + "." + extension
+    if (value.length >= distance) {
+      return value.substr(0, distance) + '...'
+    }
+    return value
   }
-
 }
