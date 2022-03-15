6e791a44256d6710857bfb0a08d801bbe7bef70f
Matin <matin.parsapour.iam@gmail.com>
2022-03-15 17:21:31 +0330

    Create a component to signing in using google account

---
+++ src/app/components/google-sign-in/google-sign-in.component.ts
@@ -0,0 +1,86 @@
+import { slideToDown } from './../../animations';
+import { Router } from '@angular/router';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { LoginService } from './../../services/login/login.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationType } from 'src/app/enum/notification-type';
+
+@Component({
+  selector: 'app-google-sign-in',
+  templateUrl: './google-sign-in.component.html',
+  styleUrls: ['./google-sign-in.component.css'],
+  animations: [slideToDown],
+})
+export class GoogleSignInComponent implements OnInit {
+  user: FormGroup;
+  socialUser!: SocialUser;
+  isLoggedin!: boolean; 
+
+  constructor(
+    private socialAuthService: SocialAuthService,
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private notifier: NotificationService,
+    private router: Router
+  ) {
+    this.user = fb.group({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      email: new FormControl(''),
+      userName: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  ngOnInit(): void {
+    this.authenticateUserForSignIn();
+  }
+
+  authenticateUserForSignIn() {
+    this.socialAuthService.authState.subscribe((user) => {
+      this.socialUser = user;
+      this.isLoggedin = user != null;
+    });
+  }
+
+  loginWithGoogle(): void {
+    this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then(() => {
+      this.initializeUser();
+      this.signInUser();
+    });
+  }
+
+  initializeUser() {
+    this.user.get('firstName')?.setValue(this.socialUser.firstName);
+    this.user.get('lastName')?.setValue(this.socialUser.lastName);
+    this.user.get('userName')?.setValue(this.socialUser.email);
+    this.user.get('email')?.setValue(this.socialUser.email);
+    this.user.get('password')?.setValue('MMmm11!!11');
+  }
+
+  updateLocalStorage(user: any) {
+    localStorage.setItem('username', user.userName);
+    localStorage.setItem('firstName', user.firstName);
+    localStorage.setItem('lastName', user.lastName);
+  }
+
+  signInUser() {
+    this.loginService.create('/sign-in', this.user.value).subscribe(
+      (response: any) => {
+        this.notifier.notify(
+          NotificationType.SUCCESS,
+          'You logged in successfully'
+        );
+        this.router.navigateByUrl('/main');
+        this.updateLocalStorage(response);
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, 'Something went wrong');
+        console.log(error);
+      }
+    );
+  }
+}
