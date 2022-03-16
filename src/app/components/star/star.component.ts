17e31e3e70ca5511b69fbca650cd1bdf335a240a
Matin <matin.parsapour.iam@gmail.com>
2022-03-16 10:28:22 +0330

    Add a component for starring a todo

---
+++ src/app/components/star/star.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit {
+
+  @Input("isStarred") isStarred!: boolean;
+  @Output("click") click = new EventEmitter() 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
