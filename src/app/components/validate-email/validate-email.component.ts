fa3e38006441621a0e65df51846415c9ba68e20f
Matin <matin.parsapour.iam@gmail.com>
2022-03-16 18:55:18 +0330

    Update typescript file

--- src/app/components/validate-email/validate-email.component.ts
+++ src/app/components/validate-email/validate-email.component.ts
@@ -1,6 +1,7 @@
 import { ValidateEmailService } from './../../services/validateEmail/validate-email.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-validate-email',
@@ -11,15 +12,29 @@ export class ValidateEmailComponent implements OnInit {
 
   email: any;
   code: any;
+  isLoading!: boolean
+  isPageValid!: boolean;
+  error!: HttpErrorResponse
 
   constructor(private route: ActivatedRoute, private router: Router, private service: ValidateEmailService) { }
 
   ngOnInit(): void {
+    this.isLoading = true
     this.route.queryParams.subscribe(params => 
       {
       this.email = params['email']
       this.code = params['code']
-      this.service.sendValidatedUserEmail(this.email, this.code)
+      this.service.sendValidatedUserEmail(this.email, this.code).subscribe(
+        (response: any) => {
+          this.isPageValid = true
+          this.isLoading = false;
+        },
+        (error: HttpErrorResponse) => {          
+          this.isPageValid = false
+          this.isLoading = false;
+          this.error = error.error;
+        }
+      );
       })
   }
 
