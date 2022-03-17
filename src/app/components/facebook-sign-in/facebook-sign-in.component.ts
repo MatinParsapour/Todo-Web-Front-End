3f97e5009675c06547afc84ea5b9711d880cff22
Matin <matin.parsapour.iam@gmail.com>
2022-03-17 11:31:33 +0330

    Delete username from facebook sign in api

--- src/app/components/facebook-sign-in/facebook-sign-in.component.ts
+++ src/app/components/facebook-sign-in/facebook-sign-in.component.ts
@@ -34,7 +34,6 @@ export class FacebookSignInComponent implements OnInit {
       firstName: new FormControl(''),
       lastName: new FormControl(''),
       email: new FormControl(''),
-      userName: new FormControl(''),
       password: new FormControl(''),
     });
   }
@@ -63,7 +62,6 @@ export class FacebookSignInComponent implements OnInit {
     this.user.get('firstName')?.setValue(this.socialUser.firstName);
     this.user.get('lastName')?.setValue(this.socialUser.lastName);
     this.user.get('email')?.setValue(this.socialUser.email);
-    this.user.get('userName')?.setValue(this.socialUser.email);
     this.user.get('password')?.setValue('MMmm11!!11');    
   }
 
