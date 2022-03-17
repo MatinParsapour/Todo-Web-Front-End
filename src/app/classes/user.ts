6dbb6402a2951e441ac1bd1807b9772cf89ed374
Matin <matin.parsapour.iam@gmail.com>
2022-03-17 11:20:55 +0330

    Delete username from user class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -2,7 +2,6 @@ export class User{
     id: number = 0;
     firstName: string = "";
     lastName: string = "";
-    userName: string = "";
     email: string = "";
     phoneNumber: number = 0;
     birthDay: any;
