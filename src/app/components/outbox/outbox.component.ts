02a6a3ebb947bade42442769a1b2cfe8a39fdac1
Matin <matin.parsapour.iam@gmail.com>
2022-03-21 17:03:23 +0330

    Update outbox component to display data

--- src/app/components/outbox/outbox.component.ts
+++ src/app/components/outbox/outbox.component.ts
@@ -1,4 +1,9 @@
+import { NotificationService } from './../../services/notification/notification.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { OutboxService } from './../../services/email-service/outbox/outbox.service';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-outbox',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./outbox.component.css']
 })
 export class OutboxComponent implements OnInit {
+  dataSource: any
 
-  constructor() { }
+  constructor(private outboxService: OutboxService,
+    private router: Router,
+    private notifier: NotificationService) { }
 
   ngOnInit(): void {
+    this.outboxService.getAllOutbox("outbox/" + localStorage.getItem("username")).subscribe(
+      (response: any) => {
+        this.dataSource = response
+        this.notifier.notify(NotificationType.SUCCESS, "Data updated")
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+        this.notifier.notify(NotificationType.ERROR, error.error)
+      }
+    )
   }
 
+  displayedColumns: string[] = ['from','to','date','message','delete']
+
+  displayData(element:any){
+    console.log(element);
+  }
+
+  backToMain(){
+    this.router.navigateByUrl("/main")
+  }
 }
--- src/app/services/email-service/outbox/outbox.service.ts
+++ src/app/services/email-service/outbox/outbox.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './../../data/data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class OutboxService {
+export class OutboxService extends DataService {
 
-  constructor() { }
+  httpService: HttpClient;
+
+  constructor(http: HttpClient) {
+    super("http://localhost:8080", http)
+    this.httpService = http;
+  }
+
+  getAllOutbox(uri: string){
+    return this.httpService.get("http://localhost:8080/user-email/" + uri)
+  }
 }
