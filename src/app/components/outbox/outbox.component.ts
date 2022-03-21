d6ad17706f50caedb0834e2aafbb1151facf688f
Matin <matin.parsapour.iam@gmail.com>
2022-03-21 21:45:40 +0330

    Update outbox and implement search

--- src/app/components/outbox/outbox.component.ts
+++ src/app/components/outbox/outbox.component.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { EmailDetailsComponent } from './../email-details/email-details.component';
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationService } from './../../services/notification/notification.service';
@@ -6,6 +7,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { OutboxService } from './../../services/email-service/outbox/outbox.service';
 import { Component, OnInit } from '@angular/core';
 import { NotificationType } from 'src/app/enum/notification-type';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-outbox',
@@ -13,7 +15,8 @@ import { NotificationType } from 'src/app/enum/notification-type';
   styleUrls: ['./outbox.component.css'],
 })
 export class OutboxComponent implements OnInit {
-  dataSource: any;
+  dataSource = new MatTableDataSource();
+  search = new FormControl('')
 
   constructor(
     private outboxService: OutboxService,
@@ -23,14 +26,21 @@ export class OutboxComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.getAllOutbox()
+    this.getAllOutbox();
   }
 
   displayedColumns: string[] = ['from', 'to', 'date', 'message'];
 
   emailDetails(element: any) {
-    this.dialog.open(EmailDetailsComponent, {data: {emailId: element}}).afterClosed().subscribe(
-      () => this.getAllOutbox())
+    this.dialog
+      .open(EmailDetailsComponent, { data: { emailId: element } })
+      .afterClosed()
+      .subscribe(() => this.getAllOutbox());
+  }
+
+  applyFilter() {
+    const filterValue = this.search.value
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   getAllOutbox() {
@@ -38,7 +48,7 @@ export class OutboxComponent implements OnInit {
       .getAllOutbox('outbox/' + localStorage.getItem('username'))
       .subscribe(
         (response: any) => {
-          this.dataSource = response;
+          this.dataSource.data = response;
           this.notifier.notify(NotificationType.SUCCESS, 'Data updated');
         },
         (error: HttpErrorResponse) => {
