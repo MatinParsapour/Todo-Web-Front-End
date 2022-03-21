dbb27225f180da9d2eb497aae80bf4d3646f83ea
Matin <matin.parsapour.iam@gmail.com>
2022-03-21 15:59:28 +0330

    Get data from server and display to user

--- src/app/components/inbox/inbox.component.ts
+++ src/app/components/inbox/inbox.component.ts
@@ -1,3 +1,6 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { InboxService } from './../../services/email-service/inbox/inbox.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inbox.component.css']
 })
 export class InboxComponent implements OnInit {
+  dataSource: any;
 
-  constructor() { }
+  constructor(private inboxService: InboxService) {}
 
   ngOnInit(): void {
+    this.inboxService
+      .getAllInbox('inbox/' + localStorage.getItem('username'))
+      .subscribe(
+        (response: any) => {
+          this.dataSource = response;
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
   }
 
+  displayedColumns: string[] = ['from', 'to', 'date', 'message'];
+
+  displayData(element: any){
+    console.log(element);
+    
+  }
 }
