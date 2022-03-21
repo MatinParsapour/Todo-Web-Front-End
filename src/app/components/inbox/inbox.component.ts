b61841258a7567c2e7bcd5e85e640124d26ead40
Matin <matin.parsapour.iam@gmail.com>
2022-03-21 17:03:03 +0330

    Use notifier to notify user

--- src/app/components/inbox/inbox.component.ts
+++ src/app/components/inbox/inbox.component.ts
@@ -1,8 +1,10 @@
+import { NotificationService } from './../../services/notification/notification.service';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
 import { InboxService } from './../../services/email-service/inbox/inbox.service';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-inbox',
@@ -12,7 +14,9 @@ import { Component, OnInit } from '@angular/core';
 export class InboxComponent implements OnInit {
   dataSource: any;
 
-  constructor(private inboxService: InboxService, private router: Router) {}
+  constructor(private inboxService: InboxService, 
+    private router: Router,
+    private notifier: NotificationService) {}
 
   ngOnInit(): void {
     this.inboxService
@@ -20,14 +24,16 @@ export class InboxComponent implements OnInit {
       .subscribe(
         (response: any) => {
           this.dataSource = response;
+          this.notifier.notify(NotificationType.SUCCESS, "Data updated")
         },
         (error: HttpErrorResponse) => {
           console.log(error);
+          this.notifier.notify(NotificationType.ERROR, error.error)
         }
       );
   }
 
-  displayedColumns: string[] = ['from', 'to', 'date', 'message'];
+  displayedColumns: string[] = ['from', 'to', 'date', 'message', 'delete'];
 
   displayData(element: any){
     console.log(element);
