ed080ed28c17d7645d4396671001ca5ac070a866
Matin <matin.parsapour.iam@gmail.com>
2022-03-23 18:18:22 +0430

    Create a component for users

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,35 @@
+import { UserManagementService } from './../../services/user-management/user-management.service';
+import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationType } from 'src/app/enum/notification-type';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent implements OnInit {
+  users: any;
+
+  constructor(
+    private userManagementService: UserManagementService,
+    private notifier: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.getAllUsers();
+  }
+
+  getAllUsers() {
+    this.userManagementService.getAll('/get-all').subscribe(
+      (response: any) => {
+        this.users = response;
+        console.log(this.users);
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error);
+      }
+    );
+  }
+}
