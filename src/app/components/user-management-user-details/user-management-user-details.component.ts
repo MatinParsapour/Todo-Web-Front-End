05eb4574c5f873f514b2650d7653b3d762759894
Matin <matin.parsapour.iam@gmail.com>
2022-03-24 11:21:33 +0430

    Show user profile image if user has in user details

--- src/app/components/user-management-user-details/user-management-user-details.component.ts
+++ src/app/components/user-management-user-details/user-management-user-details.component.ts
@@ -1,5 +1,10 @@
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationType } from 'src/app/enum/notification-type';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { User } from 'src/app/classes/user';
 
 @Component({
   selector: 'app-user-management-user-details',
@@ -8,16 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserManagementUserDetailsComponent implements OnInit {
   userId: any;
+  user!: User;
+  isLoading: boolean = false;
 
-  constructor(private activatedRoute: ActivatedRoute) {}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private notifier: NotificationService
+  ) {}
 
   ngOnInit(): void {
-    this.getUrlParameters()
+    this.getUrlParameters();
+    this.getUser()
   }
 
   getUrlParameters() {
     this.activatedRoute.queryParams.subscribe((parameter: any) => {
       this.userId = parameter['user'];
     });
+  }
+
+  getUser() {
+    this.isLoading = true;
+    this.userService.getUser(this.userId).subscribe(
+      (response: any) => {
+        this.user = response;
+        this.isLoading = false;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+        this.isLoading = false;
+
+        this.notifier.notify(
+          NotificationType.ERROR,
+          'Something went wrong by getting data, try again later'
+        );
+      }
+    );
   }
 }
