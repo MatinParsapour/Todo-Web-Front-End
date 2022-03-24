8c5616e0b3887cd5653600a09ddc9a1863aa9aa5
Matin <matin.parsapour.iam@gmail.com>
2022-03-24 20:47:23 +0430

    Add needed properties to user class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -1,10 +1,14 @@
-export class User{
-    id: number = 0;
-    firstName: string = "";
-    lastName: string = "";
-    email: string = "";
-    phoneNumber: number = 0;
-    birthDay: any;
-    age: string = "";
-    profileImageUrl: string = "";
+export class User {
+  id: number = 0;
+  firstName: string = '';
+  lastName: string = '';
+  email: string = '';
+  phoneNumber: number = 0;
+  birthDay: any;
+  role: any;
+  authorities = [];
+  isBlocked!: boolean;
+  isDeleted!: boolean;
+  age: string = '';
+  profileImageUrl: string = '';
 }
