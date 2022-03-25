0afb184a09a15cf004963cd89b370a638718d395
Matin <matin.parsapour.iam@gmail.com>
2022-03-25 10:54:49 +0430

    Add a pipe for replacing characters

---
+++ src/app/pipes/replacer.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'replacer'
+})
+export class ReplacerPipe implements PipeTransform {
+
+  transform(value: string, character?: any, changeWith?: any): any {
+    value = value.replace(character, changeWith);
+    return value;
+  }
+
+}
