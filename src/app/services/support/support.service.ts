c114bf9532db6189cbc148269badf940da5fd1b4
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 19:25:21 +0430

    Update support service

--- src/app/services/support/support.service.ts
+++ src/app/services/support/support.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SupportService {
+export class SupportService extends DataService {
 
-  constructor() { }
+  httpService: HttpClient
+
+  constructor(http : HttpClient) {
+    super("http://localhost:8080/", http);
+    this.httpService = http
+  }
 }
