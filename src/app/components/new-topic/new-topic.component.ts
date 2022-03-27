cf30af4dadabd08485656f0319b6a6dace5de737
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 19:22:31 +0430

    Update new topic component for user to set priority and topic

--- src/app/components/new-topic/new-topic.component.ts
+++ src/app/components/new-topic/new-topic.component.ts
@@ -1,3 +1,4 @@
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-topic.component.css']
 })
 export class NewTopicComponent implements OnInit {
+  request: FormGroup;
 
-  constructor() { }
+
+  constructor(private fb: FormBuilder) {
+    this.request = fb.group({
+      priority: new FormControl('', Validators.required),
+      topic: new FormControl('', [Validators.required, Validators.minLength(3)])
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  priorities = ['low','medium','high']
+
+  get priority():any{
+    return this.request.get('priority')
+  }
+
+  get topic():any{
+    return this.request.get('topic')
+  }
+
+  addNewTopic(){
+    console.log(this.request.value);
+  }
+
+  getTopicErrorMessages(){
+    if (this.topic.hasError('required')){
+      return "You need to enter topic"
+    } else if (this.topic.hasError('minlength')) {
+      return 3 - this.topic.value.length + " more characters"
+    }
+
+    return null
   }
 
 }
