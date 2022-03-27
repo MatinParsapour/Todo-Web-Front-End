eba51df2a4d2f8d548e93b435ee62022244bd604
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 19:36:56 +0430

    Update new topic component to send request to server to create new topic

--- src/app/components/new-topic/new-topic.component.ts
+++ src/app/components/new-topic/new-topic.component.ts
@@ -1,47 +1,90 @@
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { SupportService } from './../../services/support/support.service';
+import {
+  FormGroup,
+  FormBuilder,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-new-topic',
   templateUrl: './new-topic.component.html',
-  styleUrls: ['./new-topic.component.css']
+  styleUrls: ['./new-topic.component.css'],
 })
 export class NewTopicComponent implements OnInit {
   request: FormGroup;
+  isLoading = false;
 
-
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private supportService: SupportService,
+    private notifier: NotificationService,
+    private dialog: MatDialog
+  ) {
     this.request = fb.group({
       priority: new FormControl('', Validators.required),
-      topic: new FormControl('', [Validators.required, Validators.minLength(3)])
-    })
+      topic: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      userId: new FormControl(''),
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  priorities = ['low', 'medium', 'high'];
+
+  get priority(): any {
+    return this.request.get('priority');
   }
 
-  priorities = ['low','medium','high']
-
-  get priority():any{
-    return this.request.get('priority')
+  get topic(): any {
+    return this.request.get('topic');
   }
 
-  get topic():any{
-    return this.request.get('topic')
+  addNewTopic() {
+    this.setUserIdInRequest();
+    this.sendAddTopicRequest()
   }
 
-  addNewTopic(){
-    console.log(this.request.value);
-  }
-
-  getTopicErrorMessages(){
-    if (this.topic.hasError('required')){
-      return "You need to enter topic"
+  getTopicErrorMessages() {
+    if (this.topic.hasError('required')) {
+      return 'You need to enter topic';
     } else if (this.topic.hasError('minlength')) {
-      return 3 - this.topic.value.length + " more characters"
+      return 3 - this.topic.value.length + ' more characters';
     }
 
-    return null
+    return null;
   }
 
+  setUserIdInRequest() {
+    const userId = localStorage.getItem('username');
+    if (userId !== null) {
+      this.request.get('userId')?.setValue(userId);
+    }
+  }
+
+  sendAddTopicRequest() {
+    this.isLoading = true
+    this.supportService
+      .create('request/start-new-request', this.request.value)
+      .subscribe(
+        (response: any) => {
+          this.notifier.notify(NotificationType.SUCCESS, 'The topic added');
+        },
+        (error: HttpErrorResponse) => {
+          this.notifier.notify(NotificationType.ERROR, error.error);
+        },
+        () => {
+          this.dialog.closeAll()
+          this.isLoading = false
+        }
+      );
+  }
 }
