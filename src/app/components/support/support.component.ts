045269475118b6e75e4743cc16376165859edca5
Matin <matin.parsapour.iam@gmail.com>
2022-03-27 21:31:03 +0430

    Add something like a chat bar for topics user created

--- src/app/components/support/support.component.ts
+++ src/app/components/support/support.component.ts
@@ -1,7 +1,11 @@
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SupportService } from './../../services/support/support.service';
 import { Router } from '@angular/router';
 import { NewTopicComponent } from './../new-topic/new-topic.component';
 import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-support',
@@ -9,18 +13,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./support.component.css']
 })
 export class SupportComponent implements OnInit {
+  requests: any
+  isLoading = false
 
   constructor(private dialog: MatDialog, 
-    private router: Router) { }
+    private router: Router,
+    private supportService: SupportService,
+    private notifier: NotificationService) { }
 
   ngOnInit(): void {
+    this.getAllRequests()
   }
 
   openNewTopicDialog(){
-    this.dialog.open(NewTopicComponent)
+    this.dialog.open(NewTopicComponent).afterClosed().subscribe(() => this.getAllRequests())
   }
 
   backToMain(){
     this.router.navigateByUrl("/main")
+  }
+
+  getAllRequests(){
+    this.isLoading = true
+    this.supportService.getAll("request/get-user-requests/" + localStorage.getItem("username")).subscribe(
+      (response: any) => {
+        this.requests = response
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error)
+      },
+      () => {
+        this.isLoading = false
+      }
+    )
   }
 }
