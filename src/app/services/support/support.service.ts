7ab75f51632a8ba836ee953d6e43a12441adc177
Matin <matin.parsapour.iam@gmail.com>
2022-03-28 12:32:19 +0430

    Add a method in support service to get data of one request

--- src/app/services/support/support.service.ts
+++ src/app/services/support/support.service.ts
@@ -1,6 +1,8 @@
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
+import { Request } from 'src/app/classes/request';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,9 @@ export class SupportService extends DataService {
   constructor(http : HttpClient) {
     super("http://localhost:8080/", http);
     this.httpService = http
+  }
+
+  getRequest(uri: string): Observable<Request> {
+    return this.httpService.get<Request>("http://localhost:8080/" + uri)
   }
 }
