45017052158d52f53925445504bfc571fa293698
Matin <matin.parsapour.iam@gmail.com>
2022-04-01 17:51:46 +0430

    Create a component for support to see all users requests

---
+++ src/app/components/users-requests/users-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-requests',
+  templateUrl: './users-requests.component.html',
+  styleUrls: ['./users-requests.component.css']
+})
+export class UsersRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
