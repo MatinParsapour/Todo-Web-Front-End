05e613e2b2543b93ae3198503dc3d6def25aefff
Matin <matin.parsapour.iam@gmail.com>
2022-04-01 17:58:23 +0430

    Update users requests component to fetch data from server

--- src/app/components/users-requests/users-requests.component.ts
+++ src/app/components/users-requests/users-requests.component.ts
@@ -1,4 +1,8 @@
+import { NotificationService } from './../../services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { SupportService } from './../../services/support/support.service';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-users-requests',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-requests.component.css']
 })
 export class UsersRequestsComponent implements OnInit {
+  requests: any;
+  isLoading = false
 
-  constructor() { }
+  constructor(private supportService: SupportService,
+    private notifier: NotificationService) { }
 
   ngOnInit(): void {
+    this.getAllRequests()
+  }
+
+  getAllRequests(){
+    this.isLoading = true
+    this.supportService.getAll("request/get-all-users-requests").subscribe(
+      response => {
+        this.requests = response
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error)
+        console.log(error);
+        
+      },
+      () => {
+        this.isLoading = false
+      }
+    )
   }
 
 }
