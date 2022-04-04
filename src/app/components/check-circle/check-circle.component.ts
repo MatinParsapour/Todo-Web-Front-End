b9e30b7361b18d36a480016855ff16cf45b52e8e
Matin <matin.parsapour.iam@gmail.com>
2022-04-04 19:38:11 +0430

    Add a method to check if status is done or not based on status enum

--- src/app/components/check-circle/check-circle.component.ts
+++ src/app/components/check-circle/check-circle.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Status } from 'src/app/enum/status-type';
 
 @Component({
   selector: 'app-check-circle',
   templateUrl: './check-circle.component.html',
-  styleUrls: ['./check-circle.component.css']
+  styleUrls: ['./check-circle.component.css'],
 })
 export class CheckCircleComponent implements OnInit {
-
   @Input('status') status!: any;
-  @Output('click') click = new EventEmitter()
+  @Output('click') click = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isDone() {
+    return this.status.toString() === Status[Status.DONE];
   }
-
 }
