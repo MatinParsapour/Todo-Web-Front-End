1054f58550295e0bfdffe5313474ffe10f89c106
Matin <matin.parsapour.iam@gmail.com>
2022-04-29 15:04:12 +0430

    Add a method to delete a comment

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { CommentService } from './../../services/comment/comment.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.css']
+  styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent implements OnInit {
-  @Input('comment') comment: any
+  @Input('comment') comment: any;
+  @Input('todo') todo: any;
 
-  constructor() { }
+  constructor(private commentService: CommentService,
+              private notifier: NotificationService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  deleteComment() {
+    this.commentService.delete('/delete-comment/' + this.comment.id + "/" + this.todo.id).subscribe(
+      response => {
+        this.notifier.notify(NotificationType.SUCCESS, "The comment successfully deleted")
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error)
+        console.log(error);
+        
+      }
+    );
   }
-
 }
