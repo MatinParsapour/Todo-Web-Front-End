5a6c9fd529627adff41dfb69acf48b98a11877f8
Matin <matin.parsapour.iam@gmail.com>
2022-04-30 17:26:47 +0430

    Add some functions to refuse go to next line when user press enter and send data to server to edit a comment and update data dynamically

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,7 +1,7 @@
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { CommentService } from './../../services/comment/comment.service';
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, HostListener } from '@angular/core';
 import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
@@ -12,22 +12,62 @@ import { NotificationType } from 'src/app/enum/notification-type';
 export class CommentComponent implements OnInit {
   @Input('comment') comment: any;
   @Input('todo') todo: any;
+  @Output('update') update = new EventEmitter();
+  isEditable = false;
 
-  constructor(private commentService: CommentService,
-              private notifier: NotificationService) {}
+  constructor(
+    private commentService: CommentService,
+    private notifier: NotificationService
+  ) {}
 
   ngOnInit(): void {}
 
   deleteComment() {
-    this.commentService.delete('/delete-comment/' + this.comment.id + "/" + this.todo.id).subscribe(
-      response => {
-        this.notifier.notify(NotificationType.SUCCESS, "The comment successfully deleted")
-      },
-      (error: HttpErrorResponse) => {
-        this.notifier.notify(NotificationType.ERROR, error.error)
-        console.log(error);
-        
-      }
-    );
+    this.commentService
+      .delete('/delete-comment/' + this.comment.id + '/' + this.todo.id)
+      .subscribe(
+        (response) => {
+          this.notifier.notify(
+            NotificationType.SUCCESS,
+            'The comment successfully deleted'
+          );
+        },
+        (error: HttpErrorResponse) => {
+          this.notifier.notify(NotificationType.ERROR, error.error);
+          console.log(error);
+        }
+      );
+  }
+
+  toggleMessageEditable() {
+    this.isEditable = !this.isEditable;
+  }
+
+  @HostListener('window:keydown',['$event'])
+  refuseGoToNextLine(event: KeyboardEvent){
+    if (event.key === 'Enter'){
+      event.preventDefault()
+    }
+  }
+
+  editComment(event: any) {
+    if (event.innerText !== '') {
+      const formData = this.createFormData(event.innerText);
+      this.commentService.update('/edit-comment', formData).subscribe(
+        (response) => {
+          this.update.emit();
+        },
+        (error: HttpErrorResponse) => {
+          this.notifier.notify(NotificationType.ERROR, error.error);
+        }
+      );
+    }
+  }
+
+  createFormData(message: any): FormData {
+    const formData = new FormData();
+    formData.append('commentId', this.comment.id),
+      formData.append('message', message);
+    return formData;
   }
 }
