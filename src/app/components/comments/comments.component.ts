4a43a664075cf6c3b7bc3e5afd8904f520f41049
Matin <matin.parsapour.iam@gmail.com>
2022-04-30 17:27:49 +0430

    Get update method and add new method to send to child component to update data dynamically

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-comments',
@@ -7,10 +7,15 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class CommentsComponent implements OnInit {
   @Input('todo') todo: any
+  @Output('update') update = new EventEmitter()
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateComment(){
+    this.update.emit()
   }
 
 }
