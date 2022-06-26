e94750df86b0ae7545ad92a7d20133a2bcf9af20
Matin <matin.parsapour.iam@gmail.com>
2022-06-26 14:58:42 +0430

    Update settings service to contact with server

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -1,13 +1,23 @@
+import { User } from 'src/app/classes/user';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SettingsService extends DataService {
+  private httpService: HttpClient;
 
   constructor(http: HttpClient) {
-    super("http://localhost:8080/", http)
+    super('http://localhost:8080/', http);
+    this.httpService = http;
+  }
+
+  getUser(userId: any): Observable<User> {
+    return this.httpService.get<User>(
+      'http://localhost:8080/user/get-user/' + userId
+    );
   }
 }
