5ff813afc565f71c6285ed0a94430c17b6efa907
Matin <matin.parsapour.iam@gmail.com>
2022-06-26 14:35:50 +0430

    Update settings service

--- src/app/services/settings/settings.service.ts
+++ src/app/services/settings/settings.service.ts
@@ -1,9 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SettingsService {
+export class SettingsService extends DataService {
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super("http://localhost:8080/", http)
+  }
 }
