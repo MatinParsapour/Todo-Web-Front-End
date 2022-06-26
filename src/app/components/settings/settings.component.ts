1512e1db7abe9aa42b6c0eb442b1025847888f7a
Matin <matin.parsapour.iam@gmail.com>
2022-06-26 14:58:23 +0430

    Update settings compnent type script file to get user data

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { SettingsService } from './../../services/settings/settings.service';
+import { User } from 'src/app/classes/user';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +12,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+  userId: any;
+  user!: User
+  isLoading = false;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private settingsService: SettingsService,
+    private notifier: NotificationService
+  ) { }
 
   ngOnInit(): void {
+    this.userId = this.activatedRoute.snapshot.params['userId']
+    this.getUser()
+  }
+
+  getUser(){
+    this.isLoading = true
+    this.settingsService.getUser(this.userId).subscribe(
+      (response: any) => {
+        this.user = response;
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error)
+      },
+      () => {
+        this.isLoading = false
+      }
+    )
   }
 
 }
