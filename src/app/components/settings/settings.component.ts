f28576df38cc14874b488d7f7219d4c26397a772
Matin <matin.parsapour.iam@gmail.com>
2022-06-28 17:49:26 +0430

    Implement methods for user to add, update and delete profile image

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,4 +1,5 @@
-import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from './../../services/user/user.service';
+import { HttpErrorResponse, HttpEvent, HttpEventType } from '@angular/common/http';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { SettingsService } from './../../services/settings/settings.service';
 import { User } from 'src/app/classes/user';
@@ -9,50 +10,125 @@ import { NotificationType } from 'src/app/enum/notification-type';
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
   userId: any;
   settingsType: any;
-  user!: User
+  user!: User;
   isLoading = false;
+  public profileImage: any;
+  uploaded = 0;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private settingsService: SettingsService,
+    private userService: UserService,
     private notifier: NotificationService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.userId = this.activatedRoute.snapshot.params['userId']
-    this.settingsType = this.activatedRoute.snapshot.params['settingsType']
-    this.getUser()
+    this.userId = this.activatedRoute.snapshot.params['userId'];
+    this.settingsType = this.activatedRoute.snapshot.params['settingsType'];
+    this.getUser();
   }
 
-  getUser(){
-    this.isLoading = true
+  getUser() {
+    this.isLoading = true;
     this.settingsService.getUser(this.userId, this.settingsType).subscribe(
       (response: any) => {
-        this.user = response;        
+        this.user = response;
       },
       (error: HttpErrorResponse) => {
-        this.notifier.notify(NotificationType.ERROR, error.error)
+        this.notifier.notify(NotificationType.ERROR, error.error);
       },
       () => {
-        this.isLoading = false
+        this.isLoading = false;
       }
-    )
+    );
   }
 
-  isPersonalInfo():boolean {
-    return this.settingsType == "personal-info";
+  selectProfile() {
+    this.clickButton('select-picture');
   }
 
-  isSecurityInfo():boolean {
-    return this.settingsType == "security-info";
+  clickButton(tagId: any) {
+    document.getElementById(tagId)?.click();
   }
 
-  isAccountInfo():boolean {
-    return this.settingsType == "account-info";
+  changeProfileImage(event: any) {
+    this.profileImage = event.target.files[0];
+    const formData = new FormData();
+    formData.append('userId', this.userId);
+    formData.append('profileImage', this.profileImage);
+    this.userService.updateProfileImage(formData).subscribe(
+      (event: HttpEvent<any>) => {
+        this.reportUploadProgress(event);
+        this.getUser();
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
+  }
+
+  reportUploadProgress(event: HttpEvent<any>) {
+    switch (event.type) {
+      case HttpEventType.UploadProgress:
+        if (event.total) {
+          this.uploaded = (100 * event.loaded) / event.total;
+        }
+        break;
+      case HttpEventType.Response:
+        if (event.status === 200) {
+          this.user.profileImageUrl = '';
+          this.user.profileImageUrl = `${event.body.profileImageUrl}`;
+          this.user.profileImageUrl = `${
+            event.body.profileImageUrl
+          }?time=${new Date().getTime()}`;
+          this.notifier.notify(
+            NotificationType.SUCCESS,
+            'Profile image updated'
+          );
+        } else {
+          this.notifier.notify(
+            NotificationType.ERROR,
+            'Unable to upload image'
+          );
+          console.log(event);
+        }
+        break;
+      default:
+        this.uploaded = 0;
+    }
+  }
+
+  deleteProfile() {
+    this.userService
+      .delete('/user/delete-profile-image/' + localStorage.getItem('username'))
+      .subscribe(
+        (response: any) => {
+          this.notifier.notify(
+            NotificationType.SUCCESS,
+            'Your profile successfully deleted'
+          );
+          this.getUser();
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
+
+  isPersonalInfo(): boolean {
+    return this.settingsType == 'personal-info';
+  }
+
+  isSecurityInfo(): boolean {
+    return this.settingsType == 'security-info';
+  }
+
+  isAccountInfo(): boolean {
+    return this.settingsType == 'account-info';
   }
 }
