8c8eb06fdc3522a437e4e7570167eddad8ba6c39
Matin <matin.parsapour.iam@gmail.com>
2022-07-03 16:52:54 +0430

    Add a method to send user email or phone number to server

--- src/app/components/forget-username/forget-username.component.ts
+++ src/app/components/forget-username/forget-username.component.ts
@@ -1,5 +1,9 @@
+import { NotificationService } from './../../services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UserService } from './../../services/user/user.service';
 import { FormControl, Validators } from '@angular/forms';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-forget-username',
@@ -8,14 +12,37 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class ForgetUsernameComponent implements OnInit {
   @Output('close') close = new EventEmitter()
+  isLoading = false;
   isEnterEmailOrPhoneEditable = true;
   isCheckCodeEditable = false;
   isUsernameInputEditable = false;
   emailOrPhone = new FormControl('',[Validators.required, Validators.minLength(3)])
 
-  constructor() { }
+  constructor(private userService: UserService,
+              private notifier: NotificationService) { }
 
   ngOnInit(): void {
+  }
+
+  sendCode(){
+    const formData = new FormData();
+    formData.append("emailOrPhoneNumber",this.emailOrPhone.value); 
+    this.isLoading = true;
+    this.userService.create("/user/forget-username",formData).subscribe(
+      (response: any) => {
+        this.isEnterEmailOrPhoneEditable = false;
+        this.isCheckCodeEditable = true;
+        this.notifier.notify(NotificationType.SUCCESS, "Code has sent to " + this.emailOrPhone.value)
+      },
+      (error: HttpErrorResponse) => {
+        this.notifier.notify(NotificationType.ERROR, error.error)
+        console.log(error);
+        
+      },
+      () => {
+        this.isLoading = false;
+      }
+    )
   }
 
 }
