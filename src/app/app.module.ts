819614b873a6748ddc8420e3f4fb7f6cca5d02f2
Matin <matin.parsapour.iam@gmail.com>
2022-07-13 17:22:27 +0430

    Remove message component from declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -163,7 +163,6 @@ const customNotifierOptions: NotifierOptions = {
     CheckCircleComponent,
     CutterPipe,
     ReplacerPipe,
-    MessageComponent,
     SharedToDoComponent,
     FollowersFollowingsComponent,
     ExploreComponent,
