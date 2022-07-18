b4f8f5f0e71a67f83717c038ccd626bdbf37b41d
Matin <matin.parsapour.iam@gmail.com>
2022-07-18 19:52:46 +0430

    Add a service for searches

---
+++ src/app/services/search/search.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { DataService } from './../data/data.service';
+import { Injectable } from '@angular/core';
+import { Constants } from '../constant';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService extends DataService {
+
+  constructor(http: HttpClient) {
+    super(Constants.url + '/search/', http);
+  }
+}
