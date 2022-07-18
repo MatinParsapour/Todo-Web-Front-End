fe4c2fb80ec7831b9a0ea81b0db69ca0ecb7c54e
Matin <matin.parsapour.iam@gmail.com>
2022-07-18 19:53:03 +0430

    Add a property for keywords user searches

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
   username = '';
+  keyword = '';
 
   constructor(private cookieService: CookieService) {}
 
