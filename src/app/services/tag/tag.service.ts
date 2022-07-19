3ebbeabee0bb19e162cffa4242a6ab695c85cc83
Matin <matin.parsapour.iam@gmail.com>
2022-07-19 17:13:05 +0430

    Update tag service

--- src/app/services/tag/tag.service.ts
+++ src/app/services/tag/tag.service.ts
@@ -1,9 +1,16 @@
+import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../constant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class TagService {
+export class TagService extends DataService {
+  private httpService: HttpClient;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    super(Constants.url + '/tag', http);
+    this.httpService = http;
+  }
 }
