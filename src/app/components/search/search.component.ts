afb8837192a8656b24d1f7884558e65bfdf6307d
Matin <matin.parsapour.iam@gmail.com>
2022-07-19 16:38:26 +0430

    Add functionality to search type script file

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,15 +1,36 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SearchService } from 'src/app/services/search/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  keyword = '';
+  results: any[] = [];
+  constructor(private searchService: SearchService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  search(element: any) {
+    if (element.value == '') {
+      this.results = [];
+      return;
+    }
+
+    this.searchService.getAll(element.value).subscribe(
+      (response: any) => {
+        this.results = response;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error);
+      }
+    );
   }
 
+  open(id: string) {
+    console.log(id);
+  }
 }
