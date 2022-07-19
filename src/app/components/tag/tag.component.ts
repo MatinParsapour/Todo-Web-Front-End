8b0853a07b34f2078ebd25b6d84b3ceb33135cf1
Matin <matin.parsapour.iam@gmail.com>
2022-07-19 17:28:31 +0430

    Import necassary components

--- src/app/components/tag/tag.component.ts
+++ src/app/components/tag/tag.component.ts
@@ -1,4 +1,10 @@
+import { NotificationService } from './../../services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Tag } from './../../classes/tag';
+import { ActivatedRoute } from '@angular/router';
+import { TagService } from './../../services/tag/tag.service';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-tag',
