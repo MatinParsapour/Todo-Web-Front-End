6588b7015478257862f3b984e8b09bf97b5cbf33
Matin <matin.parsapour.iam@gmail.com>
2022-07-20 17:17:19 +0430

    Add AfterViewInit life cycle hook

--- src/app/components/tag/tag.component.ts
+++ src/app/components/tag/tag.component.ts
@@ -5,7 +5,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Tag } from './../../classes/tag';
 import { ActivatedRoute } from '@angular/router';
 import { TagService } from './../../services/tag/tag.service';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
