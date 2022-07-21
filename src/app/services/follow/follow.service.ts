8237c998e7717789a738020df11f964042a5c1de
Matin <matin.parsapour.iam@gmail.com>
2022-07-21 17:56:01 +0430

    Import necassary components

--- src/app/services/follow/follow.service.ts
+++ src/app/services/follow/follow.service.ts
@@ -1,4 +1,7 @@
+import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Constants } from '../constant';
 
 @Injectable({
   providedIn: 'root'
