0c14a62862ee15f196d41c206fe10fca1467a044
Matin <matin.parsapour.iam@gmail.com>
2022-07-22 17:15:47 +0430

    Import HttpErrorResponse

--- src/app/components/profile-drop-down/profile-drop-down.component.ts
+++ src/app/components/profile-drop-down/profile-drop-down.component.ts
@@ -1,6 +1,7 @@
 import { User } from './../../classes/user';
 import { Component, Input, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user/user.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile-drop-down',
