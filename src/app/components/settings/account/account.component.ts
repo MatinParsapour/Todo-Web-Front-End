81ddc9cf7d2d6433ec0b3cbd3ca86f831f5cdca6
Matin <matin.parsapour.iam@gmail.com>
2022-07-23 17:57:51 +0430

    Import all necassary components

--- src/app/components/settings/account/account.component.ts
+++ src/app/components/settings/account/account.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { NotificationService } from './../../../services/notification/notification.service';
+import { SettingsService } from './../../../services/settings/settings.service';
+import { User } from './../../../classes/user';
+import { Component, Input, OnInit } from '@angular/core';
+import { AccessLevel } from 'src/app/enum/access-level';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-account',
