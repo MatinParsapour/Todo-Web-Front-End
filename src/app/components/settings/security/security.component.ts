76c43c0d5e165d88e1bca463bbdf1791bedfd1c1
Matin <matin.parsapour.iam@gmail.com>
2022-07-23 17:47:27 +0430

    Import all necassary components

--- src/app/components/settings/security/security.component.ts
+++ src/app/components/settings/security/security.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { User } from './../../../classes/user';
+import { NotificationService } from './../../../services/notification/notification.service';
+import { SettingsService } from './../../../services/settings/settings.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
+import { Provider } from 'src/app/enum/provider';
+import { GetResetEmailComponent } from '../../get-reset-email/get-reset-email.component';
+import { PhoneNumberComponent } from '../../phone-number/phone-number.component';
+import { CodeValidatorComponent } from '../../code-validator/code-validator.component';
+import { ForgetPasswordComponent } from '../../forget-password/forget-password.component';
 
 @Component({
   selector: 'app-security',
