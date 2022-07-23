932e7628fa05a61996759939d0487407300e4484
Matin <matin.parsapour.iam@gmail.com>
2022-07-23 18:02:05 +0430

    Remove all unnecassary methods

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -27,30 +27,13 @@ import { Provider } from 'src/app/enum/provider';
   styleUrls: ['./settings.component.css'],
 })
 export class SettingsComponent implements OnInit {
-  accessLevelInfo!: { type: string; info: string };
-  accessLevelsInfo = [
-    {
-      type: 'Public',
-      info: 'By selecting this level of accessing, all of users around the world can see your ToDos',
-    },
-    {
-      type: 'Protected',
-      info: 'By selecting this level of accessing, only you and your followers can see your ToDos',
-    },
-    {
-      type: 'Private',
-      info: 'By selecting this level of accessing, only you can see your ToDos',
-    },
-  ];
   username: any;
   settingsType: any;
   user!: User;
   isLoading = false;
   public profileImage: any;
   uploaded = 0;
-  isVisible = false;
-  isForgetUsernameModalVisible = false;
-  bioLength = 300;
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -64,35 +47,13 @@ export class SettingsComponent implements OnInit {
 
   ngOnInit(): void {
     this.username = this.activatedRoute.snapshot.params['username'];
+    this.settingsService.setUsername(this.username);
     this.settingsType = this.activatedRoute.snapshot.params['settingsType'];
-    this.getUser();
+    // this.getUser();
   }
 
-  openGetResetEmailComponent() {
-    return this.dialog.open(GetResetEmailComponent);
-  }
-
-  openPhoneNumberComponent() {
-    return this.dialog
-      .open(PhoneNumberComponent, {data: {username: this.username}})
-      .afterClosed()
-      .subscribe((result) => {
-        if (result === 'send-code') {
-          this.openCodeValidatorComponent().subscribe(() => {
-            this.updateUser();
-          });
-        }
-      });
-  }
-
-  openForgetPassword() {
-    this.dialog.open(ForgetPasswordComponent);
-  }
-
-  openCodeValidatorComponent() {
-    return this.dialog
-      .open(CodeValidatorComponent, { disableClose: true })
-      .afterClosed();
+  loadComponent(component: any) {
+    component.node = this.username;
   }
 
   logout() {
@@ -140,125 +101,25 @@ export class SettingsComponent implements OnInit {
         }
       );
   }
+  //ToDo: Update getUser method and updateUser method for all components
 
-  changePhoneVisibility() {
-    this.user.phoneVisible = !this.user.phoneVisible;
-  }
-
-  changeEmailVisibility() {
-    this.user.emailVisible = !this.user.emailVisible;
-  }
-
-  getUser() {
-    this.isLoading = true;
-    this.settingsService.getUser(this.username, this.settingsType).subscribe(
-      (response: any) => {
-        this.user = response;
-      },
-      (error: HttpErrorResponse) => {
-        this.notifier.notify(
-          NotificationType.ERROR,
-          error.error.type + ': ' + error.error.message
-        );
-      },
-      () => {
-        this.isLoading = false;
-      }
-    );
-  }
-
-  displayAccessLevelInfo(value: any) {
-    if (value === 'PRIVATE') {
-      this.accessLevelInfo = this.accessLevelsInfo[2];
-    } else if (value === 'PROTECTED')
-      this.accessLevelInfo = this.accessLevelsInfo[1];
-    else if ((value = 'PUBLIC')) {
-      this.accessLevelInfo = this.accessLevelsInfo[0];
-    }
-  }
-
-  hideAccessLevelInfo() {
-    this.accessLevelInfo = { type: '', info: '' };
-  }
-
-  selectProfile() {
-    this.clickButton('select-picture');
-  }
-
-  clickButton(tagId: any) {
-    document.getElementById(tagId)?.click();
-  }
-
-  changeProfileImage(event: any) {
-    this.profileImage = event.target.files[0];
-    const formData = new FormData();
-    formData.append('userId', this.username);
-    formData.append('profileImage', this.profileImage);
-    this.userService.updateProfileImage(formData).subscribe(
-      (event: HttpEvent<any>) => {
-        this.reportUploadProgress(event);
-        this.getUser();
-      },
-      (error: HttpErrorResponse) => {
-        console.log(error);
-      }
-    );
-  }
-
-  reportUploadProgress(event: HttpEvent<any>) {
-    switch (event.type) {
-      case HttpEventType.UploadProgress:
-        if (event.total) {
-          this.uploaded = (100 * event.loaded) / event.total;
-        }
-        break;
-      case HttpEventType.Response:
-        if (event.status === 200) {
-          this.user.profileImageUrl = '';
-          this.user.profileImageUrl = `${event.body.profileImageUrl}`;
-          this.user.profileImageUrl = `${
-            event.body.profileImageUrl
-          }?time=${new Date().getTime()}`;
-          this.notifier.notify(
-            NotificationType.SUCCESS,
-            'Profile image updated'
-          );
-        } else {
-          this.notifier.notify(
-            NotificationType.ERROR,
-            'Unable to upload image'
-          );
-          console.log(event);
-        }
-        break;
-      default:
-        this.uploaded = 0;
-    }
-  }
-
-  closeForgetUsernameModal(value: any) {
-    this.isForgetUsernameModalVisible = false;
-    this.username = value;
-    this.user.userName = value;
-    this.router.navigate(['/' + value + '/settings/' + this.settingsType])
-  }
-
-  deleteProfile() {
-    this.userService
-      .delete('/user/delete-profile-image/' + localStorage.getItem('username'))
-      .subscribe(
-        (response: any) => {
-          this.notifier.notify(
-            NotificationType.SUCCESS,
-            'Your profile successfully deleted'
-          );
-          this.getUser();
-        },
-        (error: HttpErrorResponse) => {
-          console.log(error);
-        }
-      );
-  }
+  // getUser() {
+  //   this.isLoading = true;
+  //   this.settingsService.getUser(this.username, this.settingsType).subscribe(
+  //     (response: any) => {
+  //       this.user = response;
+  //     },
+  //     (error: HttpErrorResponse) => {
+  //       this.notifier.notify(
+  //         NotificationType.ERROR,
+  //         error.error.type + ': ' + error.error.message
+  //       );
+  //     },
+  //     () => {
+  //       this.isLoading = false;
+  //     }
+  //   );
+  // }
 
   updateUser() {
     this.isLoading = true;
@@ -276,55 +137,8 @@ export class SettingsComponent implements OnInit {
         },
         () => {
           this.isLoading = false;
-          this.getUser();
+          // this.getUser();
         }
       );
-  }
-
-  bio(value: any) {
-    this.bioLength = 300 - value.value.length
-    this.user.bio = value.value
-  }
-
-  isPersonalInfo(): boolean {
-    return this.settingsType == 'personal-info';
-  }
-
-  isSecurityInfo(): boolean {
-    return this.settingsType == 'security-info';
-  }
-
-  isAccountInfo(): boolean {
-    return this.settingsType == 'account-info';
-  }
-
-  changeAccessLevel(value: any) {
-    this.user.accessLevel = value.value;
-  }
-
-  isPrivate(): boolean {
-    return this.user.accessLevel.toString() == AccessLevel[AccessLevel.PRIVATE];
-  }
-
-  isProtected(): boolean {
-    return (
-      this.user.accessLevel.toString() == AccessLevel[AccessLevel.PROTECTED]
-    );
-  }
-
-  isPublic(): boolean {
-    return this.user.accessLevel.toString() == AccessLevel[AccessLevel.PUBLIC];
-  }
-
-  isUserProviderGoogle(): boolean {
-    return this.user.provider.toString() == Provider[Provider.GOOGLE]
-  }
-
-  isUserProviderFacebook(): boolean {
-    return this.user.provider.toString() == Provider[Provider.FACEBOOK]
-  }
-
-  isUserProviderEmail(): boolean {
-    return this.user.provider.toString() == Provider[Provider.EMAIL]
   }
 }
