1defe79d4ebb3ebc0236b705fa21203df26ea5d4
Matin <matin.parsapour.iam@gmail.com>
2022-07-26 17:03:06 +0430

    Import follow service and route

--- src/app/components/user-lists/user-lists.component.ts
+++ src/app/components/user-lists/user-lists.component.ts
@@ -1,4 +1,6 @@
+import { FollowService } from './../../services/follow/follow.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-lists',
