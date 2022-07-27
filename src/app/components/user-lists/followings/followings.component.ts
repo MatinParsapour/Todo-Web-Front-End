31e00fb3ea935b73662cf464359bab8886e9cc12
Matin <matin.parsapour.iam@gmail.com>
2022-07-27 17:40:18 +0430

    Import necassary components

--- src/app/components/user-lists/followings/followings.component.ts
+++ src/app/components/user-lists/followings/followings.component.ts
@@ -1,4 +1,11 @@
+import { NotificationService } from 'src/app/services/notification/notification.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { User } from 'src/app/classes/user';
+import { UserService } from 'src/app/services/user/user.service';
+import { FollowService } from './../../../services/follow/follow.service';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-followings',
