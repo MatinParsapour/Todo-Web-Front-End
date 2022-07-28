07052faa3a17c45f9a3f138a0de0e2be6173bd3d
Matin <matin.parsapour.iam@gmail.com>
2022-07-28 17:17:27 +0430

    Import necessary components

--- src/app/components/user-lists/tags/tags.component.ts
+++ src/app/components/user-lists/tags/tags.component.ts
@@ -1,4 +1,10 @@
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NotificationService } from './../../../services/notification/notification.service';
+import { UserService } from './../../../services/user/user.service';
+import { FollowService } from './../../../services/follow/follow.service';
 import { Component, OnInit } from '@angular/core';
+import { NotificationType } from 'src/app/enum/notification-type';
 
 @Component({
   selector: 'app-tags',
