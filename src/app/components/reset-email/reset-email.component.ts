954c40fdd60662b3bd045fc8bcbcc1d02062fca8
Matin <matin.parsapour.iam@gmail.com>
2022-07-30 22:26:35 +0430

    Update general style for reset-email component

--- src/app/components/reset-email/reset-email.component.ts
+++ src/app/components/reset-email/reset-email.component.ts
@@ -6,36 +6,46 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reset-email',
   templateUrl: './reset-email.component.html',
-  styleUrls: ['./reset-email.component.css']
+  styleUrls: ['./reset-email.component.css'],
 })
 export class ResetEmailComponent implements OnInit {
-
   email: any;
-  isLoading = false
-  isPageValid!: Boolean 
-  error!:HttpErrorResponse;
+  isLoading = false;
+  isPageValid!: Boolean;
+  error!: HttpErrorResponse;
 
-  constructor(private router: Router,private activatedRoute: ActivatedRoute, private validateEmailService: ValidateEmailService) { }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private validateEmailService: ValidateEmailService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
-    this.activatedRoute.queryParams.subscribe(result => {
-      this.email = result['email']
-      this.validateEmailService.isEmailValid("http://localhost:8080/email/validate-email/" + result['email'] + "/" + result['code']).subscribe(
-        (response: any) => {
-          this.isPageValid = true
-          this.isLoading = false
-        },
-        (error: HttpErrorResponse) => {
-          this.isPageValid = false;
-          this.error = error.error
-          this.isLoading = false
-        }
-      )
-    })
+    this.activatedRoute.queryParams.subscribe((result) => {
+      this.email = result['email'];
+      this.validateEmailService
+        .isEmailValid(
+          'http://localhost:8080/email/validate-email/' +
+            result['email'] +
+            '/' +
+            result['code']
+        )
+        .subscribe(
+          (response: any) => {
+            this.isPageValid = true;
+            this.isLoading = false;
+          },
+          (error: HttpErrorResponse) => {
+            this.isPageValid = false;
+            this.error = error.error;
+            this.isLoading = false;
+          }
+        );
+    });
   }
 
-  goToMainPage(){
-    this.router.navigateByUrl('/main')
+  goToMainPage() {
+    this.router.navigateByUrl('/main');
   }
 }
