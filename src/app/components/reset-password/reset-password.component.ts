38424f60e0f9c8d0437c5903c8e1bd654d34891c
Matin <matin.parsapour.iam@gmail.com>
2022-07-30 22:27:35 +0430

    Update general style for reset-password component

--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -3,7 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { slideToDown } from './../../animations';
 import { FormValidator } from './../register/FormValidator';
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, UntypedFormControl, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  UntypedFormControl,
+  Validators,
+} from '@angular/forms';
 import { ResetPasswordService } from 'src/app/services/reset-password/reset-password.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NotificationType } from 'src/app/enum/notification-type';
@@ -12,73 +17,101 @@ import { NotificationType } from 'src/app/enum/notification-type';
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css'],
-  animations: [slideToDown]
+  animations: [slideToDown],
 })
 export class ResetPasswordComponent implements OnInit {
-  resetPassword: UntypedFormGroup
+  resetPassword: UntypedFormGroup;
   email: any;
   code: any;
   isPageValid!: boolean;
-  error!: HttpErrorResponse
+  error!: HttpErrorResponse;
   isLoading: boolean = false;
 
-  constructor(fb : UntypedFormBuilder, 
+  constructor(
+    fb: UntypedFormBuilder,
     private route: ActivatedRoute,
     private resetPasswordService: ResetPasswordService,
     private notifier: NotificationService,
-    private router: Router) {
-    this.resetPassword = fb.group({
-      email: new UntypedFormControl(),
-      password: new UntypedFormControl('',[Validators.required, FormValidator.passwordIsWeak]),
-      reTypePassword: new UntypedFormControl('',[Validators.required])
-    }, 
-    {
-      validator: FormValidator.passwordsDoNotMatch
-    })
-  }
-
-  ngOnInit(): void {
-    this.route.queryParams.subscribe(params => {
-      this.email = params['email']
-      this.code = params['code']
-    })
-    this.resetPasswordService.isEmailValid(this.email, this.code).subscribe(
-      (response: any) => {
-        this.isPageValid = true
+    private router: Router
+  ) {
+    this.resetPassword = fb.group(
+      {
+        email: new UntypedFormControl(),
+        password: new UntypedFormControl('', [
+          Validators.required,
+          FormValidator.passwordIsWeak,
+        ]),
+        reTypePassword: new UntypedFormControl('', [Validators.required]),
       },
-      (error: HttpErrorResponse) => {
-        this.isPageValid = false;
-        this.error = error.error
-        
+      {
+        validator: FormValidator.passwordsDoNotMatch,
       }
     );
   }
 
-  get password(): any{return this.resetPassword.get('password')}
-  get reTypePassword(): any{return this.resetPassword.get('reTypePassword')}
-  get isMinimum(){ return FormValidator.isMinimum}
-  get hasNumber(){ return FormValidator.hasNumber}
-  get hasLowerCase(){ return FormValidator.hasLowerCase}
-  get hasUpperCase(){ return FormValidator.hasUpperCase}
-  get hasSpecialCharacters(){ return FormValidator.hasSpecialCharacters}
-
-  getPasswordErrorMessages(){
-    if (this.password.hasError('required')) {return "You must enter value"}
-    return "Create a stronger password"
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      this.email = params['email'];
+      this.code = params['code'];
+    });
+    this.resetPasswordService.isEmailValid(this.email, this.code).subscribe(
+      (response: any) => {
+        this.isPageValid = true;
+      },
+      (error: HttpErrorResponse) => {
+        this.isPageValid = false;
+        this.error = error.error;
+      }
+    );
   }
 
-  changePassword(){
-    this.resetPassword.get('email')?.setValue(this.email)
+  get password(): any {
+    return this.resetPassword.get('password');
+  }
+  get reTypePassword(): any {
+    return this.resetPassword.get('reTypePassword');
+  }
+  get isMinimum() {
+    return FormValidator.isMinimum;
+  }
+  get hasNumber() {
+    return FormValidator.hasNumber;
+  }
+  get hasLowerCase() {
+    return FormValidator.hasLowerCase;
+  }
+  get hasUpperCase() {
+    return FormValidator.hasUpperCase;
+  }
+  get hasSpecialCharacters() {
+    return FormValidator.hasSpecialCharacters;
+  }
+
+  getPasswordErrorMessages() {
+    if (this.password.hasError('required')) {
+      return 'You must enter value';
+    }
+    return 'Create a stronger password';
+  }
+
+  changePassword() {
+    this.resetPassword.get('email')?.setValue(this.email);
     this.isLoading = true;
-    this.resetPasswordService.update("change-password", this.resetPassword.value).subscribe(
-      (response:any) => {
-        this.notifier.notify(NotificationType.SUCCESS, "Your password has been changed")
-        this.isLoading = false;
-        this.router.navigateByUrl("/login")
-      }, 
-      (error:HttpErrorResponse) => {
-        this.notifier.notify(NotificationType.ERROR, error.message)
-        this.isLoading = false
-      })
+    this.resetPasswordService
+      .update('change-password', this.resetPassword.value)
+      .subscribe(
+        (response: any) => {
+          this.notifier.notify(
+            NotificationType.SUCCESS,
+            'Your password has been changed'
+          );
+          this.isLoading = false;
+          this.router.navigateByUrl('/login');
+        },
+        (error: HttpErrorResponse) => {
+          this.notifier.notify(NotificationType.ERROR, error.message);
+          this.isLoading = false;
+        }
+      );
   }
 }
