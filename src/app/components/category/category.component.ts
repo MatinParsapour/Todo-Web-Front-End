0af0388f6b207462a4502e58002c7bdc5dc8c52d
Matin <matin.parsapour.iam@gmail.com>
2022-07-30 21:59:30 +0430

    Update general style for category component

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -3,17 +3,13 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
+  @Input('name') name = '';
+  @Output('click') click = new EventEmitter();
 
-  @Input('name') name = "";
-  @Output('click') click = new EventEmitter()
+  constructor() {}
 
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
