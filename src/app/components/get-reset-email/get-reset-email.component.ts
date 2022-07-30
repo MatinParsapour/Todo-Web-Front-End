8a65160cc47e090d45698b1ec7d194b00e81e315
Matin <matin.parsapour.iam@gmail.com>
2022-07-30 22:20:12 +0430

    Remove redundant imoprts and update general styles reset-email component

--- src/app/components/get-reset-email/get-reset-email.component.ts
+++ src/app/components/get-reset-email/get-reset-email.component.ts
@@ -2,7 +2,12 @@ import { MatDialog } from '@angular/material/dialog';
 import { NotificationService } from './../../services/notification/notification.service';
 import { ForgetPasswordService } from './../../services/forget-password/forget-password.service';
 import { IsEmailExists } from './is-email-exists.validator';
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { NotificationType } from 'src/app/enum/notification-type';
@@ -16,11 +21,13 @@ export class GetResetEmailComponent implements OnInit {
   field: UntypedFormGroup;
   isLoading: boolean = false;
 
-  constructor(fb: UntypedFormBuilder, 
+  constructor(
+    fb: UntypedFormBuilder,
     private isEmailExists: IsEmailExists,
     private dialog: MatDialog,
     private forgetPasswordService: ForgetPasswordService,
-    private notifier: NotificationService) {
+    private notifier: NotificationService
+  ) {
     this.field = fb.group({
       email: new UntypedFormControl(
         '',
@@ -32,45 +39,45 @@ export class GetResetEmailComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  get email():any {return this.field.get('email')}
+  get email(): any {
+    return this.field.get('email');
+  }
 
-  getEmailErrorMessages(){
+  getEmailErrorMessages() {
     if (this.email.hasError('required')) {
-      return "You must enter a value"
+      return 'You must enter a value';
     }
     if (this.email.hasError('emailExists')) {
       return 'Email already exists';
     }
-    return "The email is not valid"
+    return 'The email is not valid';
   }
 
-  sendEmail(){
-    const formData = new FormData()
-    let username = localStorage.getItem("username")
+  sendEmail() {
+    const formData = new FormData();
+    let username = localStorage.getItem('username');
     if (username !== null) {
-      formData.append("userId",username)
+      formData.append('userId', username);
     }
-    formData.append("newEmail",this.email.value)
-        this.isLoading = true;
-        this.forgetPasswordService
-          .sendResetEmail(formData)
-          .subscribe(
-            (response: any) => {
-              this.isLoading = false;
-              this.notifier.notify(
-                NotificationType.SUCCESS,
-                'Email sent to ' + this.email.value
-              );
-              this.closeDialog();
-            },
-            (error: HttpErrorResponse) => {
-              this.isLoading = false;
-              this.notifier.notify(NotificationType.ERROR, error.message);
-            }
-          );
+    formData.append('newEmail', this.email.value);
+    this.isLoading = true;
+    this.forgetPasswordService.sendResetEmail(formData).subscribe(
+      (response: any) => {
+        this.isLoading = false;
+        this.notifier.notify(
+          NotificationType.SUCCESS,
+          'Email sent to ' + this.email.value
+        );
+        this.closeDialog();
+      },
+      (error: HttpErrorResponse) => {
+        this.isLoading = false;
+        this.notifier.notify(NotificationType.ERROR, error.message);
+      }
+    );
   }
 
-  closeDialog(){
-    this.dialog.closeAll()
+  closeDialog() {
+    this.dialog.closeAll();
   }
 }
--- src/app/components/get-reset-email/is-email-exists.validator.ts
+++ src/app/components/get-reset-email/is-email-exists.validator.ts
@@ -1,26 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpErrorResponse } from '@angular/common/http';
 import { RegisterService } from './../../services/register/register.service';
-import { AbstractControl, AsyncValidator, ValidationErrors } from '@angular/forms';
-import { map, Observable } from 'rxjs';
+import {
+  AbstractControl,
+  AsyncValidator,
+} from '@angular/forms';
+import { map } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
 export class IsEmailExists implements AsyncValidator {
+  constructor(private registerService: RegisterService) {}
 
-    constructor(private registerService: RegisterService){}
-
-    validate = (control: AbstractControl) => {
-        return this.registerService.isEmailDoplicate(control.value).pipe(
-          map(
-            (response: any) => {
-              if (response === true) {
-                return { emailExists: true };
-              }
-              return null;
-            }
-        ));
-    }
-    
+  validate = (control: AbstractControl) => {
+    return this.registerService.isEmailDoplicate(control.value).pipe(
+      map((response: any) => {
+        if (response === true) {
+          return { emailExists: true };
+        }
+        return null;
+      })
+    );
+  };
 }
