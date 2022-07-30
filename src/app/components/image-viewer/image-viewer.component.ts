20816800af121678315966a3be07ccf88a8f40fb
Matin <matin.parsapour.iam@gmail.com>
2022-07-30 22:21:06 +0430

    Update general style for image-viewser component

--- src/app/components/image-viewer/image-viewer.component.ts
+++ src/app/components/image-viewer/image-viewer.component.ts
@@ -3,22 +3,19 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-image-viewer',
   templateUrl: './image-viewer.component.html',
-  styleUrls: ['./image-viewer.component.css']
+  styleUrls: ['./image-viewer.component.css'],
 })
 export class ImageViewerComponent implements OnInit {
-
   @Input('open') open: boolean = false;
-  @Input('imageUrl') imageUrl = "";
-  @Output('click') click = new EventEmitter()
-  @Output('delete') delete = new EventEmitter()
+  @Input('imageUrl') imageUrl = '';
+  @Output('click') click = new EventEmitter();
+  @Output('delete') delete = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  deleteProfile() {
+    this.delete.next('');
   }
-
-  deleteProfile(){
-    this.delete.next('')
-  }
-
 }
