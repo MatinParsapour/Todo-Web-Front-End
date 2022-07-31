caf87a17571f2fe7370ed342cc5741a13e5537d2
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:40:50 +0430

    Remove redundnat imports and update general style fo settings component

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,25 +1,6 @@
-import { CookieService } from 'ngx-cookie-service';
-import { MatDialog } from '@angular/material/dialog';
-import { slideToDown } from './../../animations';
-import { UserService } from './../../services/user/user.service';
-import {
-  HttpErrorResponse,
-  HttpEvent,
-  HttpEventType,
-} from '@angular/common/http';
-import { NotificationService } from 'src/app/services/notification/notification.service';
 import { SettingsService } from './../../services/settings/settings.service';
-import { User } from 'src/app/classes/user';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { NotificationType } from 'src/app/enum/notification-type';
-import { AccessLevel } from 'src/app/enum/access-level';
-import { GetResetEmailComponent } from '../get-reset-email/get-reset-email.component';
-import { PhoneNumberComponent } from '../phone-number/phone-number.component';
-import { CodeValidatorComponent } from '../code-validator/code-validator.component';
-import { ForgetPasswordComponent } from '../forget-password/forget-password.component';
-import { AggreementComponent } from '../aggreement/aggreement.component';
-import { Provider } from 'src/app/enum/provider';
 
 @Component({
   selector: 'app-settings',
@@ -28,10 +9,10 @@ import { Provider } from 'src/app/enum/provider';
 })
 export class SettingsComponent implements OnInit {
   username: any;
-  
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private settingsService: SettingsService,
+    private settingsService: SettingsService
   ) {}
 
   ngOnInit(): void {
