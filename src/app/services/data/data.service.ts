8a740bc3c7d77405f8ca72fed98a84fb0c0847ba
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:51:58 +0430

    Update general styles for data service

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -2,25 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataService {
+  constructor(@Inject(String) private url: string, private http: HttpClient) {}
 
-  constructor(@Inject(String) private url: string, private http: HttpClient) { }
-
-  getAll(uri: string){
-    return this.http.get(this.url + uri)
+  getAll(uri: string) {
+    return this.http.get(this.url + uri);
   }
 
-  create(uri: string, object: any){
-    return this.http.post(this.url + uri, object)
+  create(uri: string, object: any) {
+    return this.http.post(this.url + uri, object);
   }
 
-  update(uri: string, object: any){
-    return this.http.put(this.url + uri, object)
+  update(uri: string, object: any) {
+    return this.http.put(this.url + uri, object);
   }
 
-  delete( uri: string, deleteObject?: any){
-    return this.http.delete(this.url + uri, deleteObject)
+  delete(uri: string, deleteObject?: any) {
+    return this.http.delete(this.url + uri, deleteObject);
   }
 }
