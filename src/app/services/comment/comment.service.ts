22f3fb66fc82a521bdb5be20f7363c77f85f9347
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:51:36 +0430

    Update general styles for comment service

--- src/app/services/comment/comment.service.ts
+++ src/app/services/comment/comment.service.ts
@@ -4,14 +4,13 @@ import { Injectable } from '@angular/core';
 import { Constants } from '../constant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CommentService extends DataService{
-
-  private httpService: HttpClient
+export class CommentService extends DataService {
+  private httpService: HttpClient;
 
   constructor(http: HttpClient) {
-    super(Constants.url + "/comment", http);
+    super(Constants.url + '/comment', http);
     this.httpService = http;
-   }
+  }
 }
