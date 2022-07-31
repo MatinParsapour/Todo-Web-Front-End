867187cb44ff6c06120160452061dce9d797f724
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:42:26 +0430

    Update general styles for star component

--- src/app/components/star/star.component.ts
+++ src/app/components/star/star.component.ts
@@ -3,16 +3,13 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  styleUrls: ['./star.component.css'],
 })
 export class StarComponent implements OnInit {
+  @Input('isStarred') isStarred!: boolean;
+  @Output('click') click = new EventEmitter();
 
-  @Input("isStarred") isStarred!: boolean;
-  @Output("click") click = new EventEmitter() 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
