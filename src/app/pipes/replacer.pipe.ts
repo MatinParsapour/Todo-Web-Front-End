cb4f5f6631e004cb0915e1b57ce354e5166189aa
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:50:41 +0430

    Update general styles for replacer pipie

--- src/app/pipes/replacer.pipe.ts
+++ src/app/pipes/replacer.pipe.ts
@@ -1,13 +1,11 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'replacer'
+  name: 'replacer',
 })
 export class ReplacerPipe implements PipeTransform {
-
   transform(value: string, character?: any, changeWith?: any): any {
     value = value.replace(character, changeWith);
     return value;
   }
-
 }
