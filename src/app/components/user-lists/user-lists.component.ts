6b955e3b5ac52d60064d853b617467c6ef467ffd
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:47:04 +0430

    Update general styles for user-lists component

--- src/app/components/user-lists/user-lists.component.ts
+++ src/app/components/user-lists/user-lists.component.ts
@@ -5,18 +5,20 @@ import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-user-lists',
   templateUrl: './user-lists.component.html',
-  styleUrls: ['./user-lists.component.css']
+  styleUrls: ['./user-lists.component.css'],
 })
 export class UserListsComponent implements OnInit {
-  constructor(private route: ActivatedRoute,
-              private followService: FollowService,
-              private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private followService: FollowService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.followService.setUsername(this.route.snapshot.params['username']);
   }
 
-  close(){
+  close() {
     this.router.navigate([this.route.snapshot.params['username']]);
   }
 }
