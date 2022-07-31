6839fb84aedbd5a40f69331c81e547643d7a466d
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:49:51 +0430

    Update general styles for cutter pipe

--- src/app/pipes/cutter.pipe.ts
+++ src/app/pipes/cutter.pipe.ts
@@ -1,17 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'cutter'
+  name: 'cutter',
 })
 export class CutterPipe implements PipeTransform {
-
   transform(value: string, pos?: any, count?: any): any {
     if (pos === 'first') {
       value = value.substring(count);
     } else if (pos === 'last') {
-      value = value.substring(0,count)
+      value = value.substring(0, count);
     }
     return value;
   }
-
 }
