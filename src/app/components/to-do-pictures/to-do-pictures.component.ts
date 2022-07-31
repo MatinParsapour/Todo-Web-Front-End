ebd8c9ad4474add0f70eafa296304c09bdc40ff8
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:44:35 +0430

    Update general styles for to-do-pictures component

--- src/app/components/to-do-pictures/to-do-pictures.component.ts
+++ src/app/components/to-do-pictures/to-do-pictures.component.ts
@@ -4,19 +4,16 @@ import { Component, OnInit, Inject } from '@angular/core';
 @Component({
   selector: 'app-to-do-pictures',
   templateUrl: './to-do-pictures.component.html',
-  styleUrls: ['./to-do-pictures.component.css']
+  styleUrls: ['./to-do-pictures.component.css'],
 })
 export class ToDoPicturesComponent implements OnInit {
-
   pictures: Array<string>;
   toDoId: any;
 
   constructor(@Inject(MAT_DIALOG_DATA) data: any) {
-    this.pictures = data.pictures
-    this.toDoId = data.toDoId
+    this.pictures = data.pictures;
+    this.toDoId = data.toDoId;
   }
 
-  ngOnInit(): void {   
-  }
-
+  ngOnInit(): void {}
 }
