2aa18bf4419df2585774e776c16ae015ffa19207
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:47:56 +0430

    Update general styles for drop-down directive

--- src/app/directives/drop-down/drop-down.directive.ts
+++ src/app/directives/drop-down/drop-down.directive.ts
@@ -1,20 +1,27 @@
-import { Directive, ElementRef, HostListener, Renderer2, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  Input,
+} from '@angular/core';
 
 @Directive({
-  selector: '[appDropDown]'
+  selector: '[appDropDown]',
 })
 export class DropDownDirective {
-  @Input('selectedClass') class: any
+  @Input('selectedClass') class: any;
 
-  constructor(private elementRef: ElementRef,
-              private renderer: Renderer2) { }
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   @HostListener('document:click', ['$event']) click(event: Event) {
-    var bindClass = this.elementRef.nativeElement.contains(event.target) ? this.class : ''
+    var bindClass = this.elementRef.nativeElement.contains(event.target)
+      ? this.class
+      : '';
     if (bindClass) {
-      this.renderer.addClass(this.elementRef.nativeElement, bindClass)
+      this.renderer.addClass(this.elementRef.nativeElement, bindClass);
     } else {
-      this.renderer.removeClass(this.elementRef.nativeElement, this.class)
+      this.renderer.removeClass(this.elementRef.nativeElement, this.class);
     }
   }
 }
