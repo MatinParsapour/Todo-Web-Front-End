69425619384a401987e888ae3ee362cfd3efb8ca
Matin <matin.parsapour.iam@gmail.com>
2022-07-31 07:47:31 +0430

    Update general styles for validate-email component

--- src/app/components/validate-email/validate-email.component.ts
+++ src/app/components/validate-email/validate-email.component.ts
@@ -6,40 +6,41 @@ import { HttpErrorResponse } from '@angular/common/http';
 @Component({
   selector: 'app-validate-email',
   templateUrl: './validate-email.component.html',
-  styleUrls: ['./validate-email.component.css']
+  styleUrls: ['./validate-email.component.css'],
 })
 export class ValidateEmailComponent implements OnInit {
-
   email: any;
   code: any;
-  isLoading!: boolean
+  isLoading!: boolean;
   isPageValid!: boolean;
-  error!: HttpErrorResponse
+  error!: HttpErrorResponse;
 
-  constructor(private route: ActivatedRoute, private router: Router, private service: ValidateEmailService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: ValidateEmailService
+  ) {}
 
   ngOnInit(): void {
-    this.isLoading = true
-    this.route.queryParams.subscribe(params => 
-      {
-      this.email = params['email']
-      this.code = params['code']
+    this.isLoading = true;
+    this.route.queryParams.subscribe((params) => {
+      this.email = params['email'];
+      this.code = params['code'];
       this.service.sendValidatedUserEmail(this.email, this.code).subscribe(
         (response: any) => {
-          this.isPageValid = true
+          this.isPageValid = true;
           this.isLoading = false;
         },
-        (error: HttpErrorResponse) => {          
-          this.isPageValid = false
+        (error: HttpErrorResponse) => {
+          this.isPageValid = false;
           this.isLoading = false;
           this.error = error.error;
         }
       );
-      })
+    });
   }
 
-  goToLogInPage(){
-    this.router.navigateByUrl("/login")
+  goToLogInPage() {
+    this.router.navigateByUrl('/login');
   }
-
 }
