6850357bcc3246e70a8be9b974871b60d8a52fa5
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 09:54:50 +0430

    Update general style for notification service

--- src/app/services/notification/notification.service.ts
+++ src/app/services/notification/notification.service.ts
@@ -1,16 +1,14 @@
-
 import { Injectable } from '@angular/core';
 import { NotifierService } from 'angular-notifier';
 import { NotificationType } from './../../enum/notification-type';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotificationService {
+  constructor(private notifier: NotifierService) {}
 
-  constructor(private notifier: NotifierService) { }
-
-  public notify(type: NotificationType, message: string){
-    this.notifier.notify(type, message)
+  public notify(type: NotificationType, message: string) {
+    this.notifier.notify(type, message);
   }
 }
