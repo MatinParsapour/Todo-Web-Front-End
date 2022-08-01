06278f6719066183b3da7a311a43ee67691a5bcc
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 09:55:32 +0430

    Update general style for reset password service

--- src/app/services/reset-password/reset-password.service.ts
+++ src/app/services/reset-password/reset-password.service.ts
@@ -4,20 +4,20 @@ import { Injectable } from '@angular/core';
 import { Constants } from '../constant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ResetPasswordService extends DataService {
-
-  httpService: HttpClient
+  httpService: HttpClient;
 
   constructor(http: HttpClient) {
-    super(Constants.url + "/email/", http);
+    super(Constants.url + '/email/', http);
     this.httpService = http;
   }
 
-  isEmailValid(email:any, code:any){
+  isEmailValid(email: any, code: any) {
     return this.httpService.get(
-      Constants.url + '/email/verify-email-for-reset-password/' +
+      Constants.url +
+        '/email/verify-email-for-reset-password/' +
         email +
         '/' +
         code
