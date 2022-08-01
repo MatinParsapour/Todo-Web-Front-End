48430cea1f882b78dd22de818e0a18b81234d9d0
Matin <matin.parsapour.iam@gmail.com>
2022-08-01 09:57:56 +0430

    Remove redundant imports and update general style

--- src/app/services/validateEmail/validate-email.service.ts
+++ src/app/services/validateEmail/validate-email.service.ts
@@ -1,25 +1,26 @@
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { DataService } from './../data/data.service';
 import { Injectable } from '@angular/core';
 import { Constants } from '../constant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ValidateEmailService extends DataService {
+  httpService: HttpClient;
 
-  httpService: HttpClient
-  
   constructor(http: HttpClient) {
-    super(Constants.url + "/email/", http);
-    this.httpService = http
+    super(Constants.url + '/email/', http);
+    this.httpService = http;
   }
 
-  sendValidatedUserEmail(email: any, code: any){
-    return this.httpService.get(Constants.url + '/email/verify-email/' + email + "/" + code)
+  sendValidatedUserEmail(email: any, code: any) {
+    return this.httpService.get(
+      Constants.url + '/email/verify-email/' + email + '/' + code
+    );
   }
 
-  isEmailValid(url:string){
-    return this.httpService.get(url)
+  isEmailValid(url: string) {
+    return this.httpService.get(url);
   }
 }
